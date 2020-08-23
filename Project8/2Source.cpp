dfc798832d453b4a99378c0d744b525b0eea0637
Jirayut Senanorit <araeize_468_arae@hotmail.com>
2020-08-23 14:57:43 +0700

    add error

--- Project8/2Source.cpp
+++ Project8/2Source.cpp
@@ -3,7 +3,10 @@ int main()
 {
 	int level;
 	scanf_s("%d", &level);
-
+	if (level < 0)
+	{
+		printf("Error");
+	}
 	for (int row = 0;row <= level;row++)
 	{
 		for (int space = level - row;space > 0;space--)
